7e6fc3da6a6276b80b5483bab42a6345acb83dfc
kka3009k <melis.babaev.su@gmail.com>
2020-07-10 15:21:27 +0600

    Add Nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+
+const appRoutes: Routes =[
+  { path: '', component: MainComponent},
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+@Component({
+  selector: 'app-nav',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  ngOnInit() {
+    alert("2");
+  }
+}
