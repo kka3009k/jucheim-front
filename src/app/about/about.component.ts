dac9dfce916614da02d51e264ca601f29475179a
kka3009k <melis.babaev.su@gmail.com>
2020-07-13 11:06:45 +0600

    Add About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 @Component({
-  selector: 'app-main',
+  selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
