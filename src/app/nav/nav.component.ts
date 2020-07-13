b97e7bd4f531fedcec38d0a8f66337e71664d9ed
kka3009k <melis.babaev.su@gmail.com>
2020-07-13 10:17:03 +0600

    Add Nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,13 +6,24 @@ import {ActivatedRoute, Router, ParamMap} from '@angular/router';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  products = [];
+  showProducts;
   constructor( public router: Router,
              ) {
   }
 
   ngOnInit() {
+    let product = {
+      id: null,
+      name: null
+    }
     this.router.navigate(['/main']);
+    product.id = 1;
+    product.name = 'Байбай';
+    this.products.push(product);
+    product.id = 2;
+    product.name = 'Канапур';
+    this.products.push(product);
   }
   routLink(link){
     this.router.navigate(['/main']);
